c09f113da862c873cfb709536b91907c3eb67218
Alejandro Garcia <alejandrogg84@gmail.com>
2012-05-04 00:52:49 +0200

    Change from panorama to pivot ApplicationBar added with two buttons and one menu New PayMe page separated from rest

--- PayMe/NewPayMePage.xaml.cs
+++ PayMe/NewPayMePage.xaml.cs
@@ -10,24 +10,74 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
-using System.Windows.Navigation;
 using Microsoft.Phone.Shell;
 
 namespace PayMe
 {
-    public partial class MainPage : PhoneApplicationPage
+    public partial class NewPayMePage : PhoneApplicationPage
     {
-        // Constructor
-        public MainPage()
+        public NewPayMePage()
         {
-            InitializeComponent();			
-            this.payMeList.DataContext = App.PayMeList;
-            this.Loaded +=new RoutedEventHandler(MainPage_Loaded);
-
-            createApplicationBar();			
+            InitializeComponent();
+			
+			createApplicationBar();
         }
-
-        private void createApplicationBar()
+		
+		//The foreground color of the text in TitleInput is set to Magenta when TitleInput
+		//gets focus.
+		private void TextInputTitle_GotFocus(object sender, RoutedEventArgs e)
+		{
+			if (TitleInput.Text == "Title")
+			{
+				TitleInput.Text = "";
+				TitleInput.Foreground = new SolidColorBrush(ApplicationConstants.inputTextColor);
+			}
+		}
+		
+		//The foreground color of the text in WatermarkTB is set to Blue when WatermarkTB
+		//loses focus. Also, if TitleInput loses focus and no text is entered, the
+		//text "Title" is displayed.
+		private void TextInputTitle_LostFocus(object sender, RoutedEventArgs e)
+		{
+			if (TitleInput.Text == String.Empty)
+			{
+				TitleInput.Text = "Title";
+				TitleInput.Foreground = new SolidColorBrush(ApplicationConstants.placeholderColor);
+			}
+		}
+		
+		//The foreground color of the text in AmountInput is set to Magenta when AmountInput
+		//gets focus.
+		private void TextInputAmount_GotFocus(object sender, RoutedEventArgs e)
+		{
+			if (AmountInput.Text == "Amount")
+			{
+				AmountInput.Text = "";
+				AmountInput.Foreground = new SolidColorBrush(ApplicationConstants.inputTextColor);
+			}
+		}
+		
+		//The foreground color of the text in AmountInput is set to Blue when AmountInput
+		//loses focus. Also, if AmountInput loses focus and no text is entered, the
+		//text "Amount" is displayed.
+		private void TextInputAmount_LostFocus(object sender, RoutedEventArgs e)
+		{
+			if (AmountInput.Text == String.Empty)
+			{
+				AmountInput.Text = "Amount";
+				AmountInput.Foreground = new SolidColorBrush(ApplicationConstants.placeholderColor);
+			}
+		}
+		
+		private void CreatePayMe(object sender, RoutedEventArgs e) {
+			if (this.TitleInput.Text != String.Empty &&
+				this.AmountInput.Text != String.Empty)
+			{
+				App.PayMeList.PayMes.Add(new PayMeItemViewModel(this.TitleInput.Text, 10, Convert.ToDouble(this.AmountInput.Text)));
+			}
+		}
+		
+		private void createApplicationBar()
         {
             //Create the ApplicationBar
             ApplicationBar = new ApplicationBar();
@@ -52,17 +102,8 @@ namespace PayMe
             //Add the menu items to the Application Bar
             ApplicationBar.MenuItems.Add(settingsMenuItem);
         }
-
-        // Cargar datos para los elementos de ViewModel
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (!App.PayMeList.IsDataLoaded)
-            {
-                App.PayMeList.LoadData();
-            }
-        }
-
-        private void newButton_Click(object sender, EventArgs e)
+		
+		private void newButton_Click(object sender, EventArgs e)
         {
             NavigationService.Navigate(new Uri("/NewPayMePage.xaml", UriKind.RelativeOrAbsolute));
         }
